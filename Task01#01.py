884cb751bf77c9886589b082f48f14ccb72ebd69
ChiChenHuang <nejiyhc@gmail.com>
2023-03-19 10:07:17 +0800

    Create Task01#01.py

---
+++ Task01#01.py
@@ -0,0 +1,21 @@
+Task #01 作業 01【實作題】Hello World
+繳交作業直接貼上完整的 Python 程式碼即可（不用上傳 .py 檔案），不用附上題目、標題自訂：
+
+#練習：在開始正式課程之前，請大家先熟悉 Python 開發環境與語法。試著在畫面上印出「Hello World，_______」，底線部分由使用者自行輸入。
+
+❏ Sample Input：Python ← 使用者輸入
+
+❏ Sample Output：Hello World，Python ← 畫面輸出
+
+❏ Hint： 變數跟字串間可以利用「 , 」或「 + 」連接起來
+
+❏ Sample Code：
+
+s = input()
+
+'''
+Your Code
+'''
+本題要求：請把 ''' Your Code ''' 三行註解的內容刪掉改成你的部分。
+
+---
